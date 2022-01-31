5747cf6ef5cd0ef2b67d4a5a8374bf8378d8082f
artoryfagottino <artorufagottino@gmail.com>
2022-01-31 23:02:12 +0300

    Add falder with C# program in ControlWorkWithMd

---
+++ ControlworkC#/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] FirstArray = { "MacBook", "777", "_Ж", "просто текст", "GeekBrains", "Bobby", "1&!", "go", "hello man" };
+int n = 0;
+
+for (int x = 0; FirstArray.Length > x; x++)
+{
+    if (FirstArray[x].Length <= 3)
+    {
+        n = n+1;
+    }
+}
+
+string[] SecondArray = new string[n];
+
+int i = 0;
+
+for (int x = 0; FirstArray.Length > x; x++)
+{
+    if (FirstArray[x].Length <= 3)
+    {
+        SecondArray[i] = FirstArray[x];
+        i++;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    for (int j = 0; j < count; j++)
+    {
+        Console.Write($"{array[j]}" + "  ");
+    }
+    Console.WriteLine();
+}
+PrintArray(SecondArray);
+
